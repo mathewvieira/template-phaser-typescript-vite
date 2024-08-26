e2e9515e6031275937406275210524662889b6e3
Mathew Vieira <47778859+mathewvieira@users.noreply.github.com>
2024-08-26 22:38:34 +0000

    Adicionando configurações ao projeto

--- replit.nix
+++ replit.nix
@@ -1,6 +1,9 @@
 { pkgs }: {
 	deps = [
     pkgs.yarn
+    pkgs.nano
+    pkgs.openssh
+    pkgs.nodePackages.prettier
     pkgs.nodePackages.typescript
     pkgs.nodePackages.typescript-language-server
   ];
