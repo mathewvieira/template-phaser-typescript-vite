67c4d0a959fef1cc718e4106802fb33cf9caddd2
Mathew Vieira <47778859+mathewvieira@users.noreply.github.com>
2024-08-26 22:48:30 +0000

    Adicionando outras configurações ao projeto

--- src/scenes/Boot.ts
+++ src/scenes/Boot.ts
@@ -1,20 +1,20 @@
-import { Scene } from "phaser";
+import { Scene } from 'phaser'
 
 export class Boot extends Scene {
     constructor() {
-        super("Boot");
+        super('Boot')
     }
 
     preload() {
         //  The Boot Scene is typically used to load in any assets you require for your Preloader, such as a game logo or background.
         //  The smaller the file size of the assets, the better, as the Boot Scene itself has no preloader.
-        
-        this.load.setPath("assets");
-        
-        this.load.image("background", "img/bg.png");
+
+        this.load.setPath('assets')
+
+        this.load.image('background', 'img/bg.png')
     }
 
     create() {
-        this.scene.start("Preloader");
+        this.scene.start('Preloader')
     }
 }
